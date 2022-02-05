d7d6b91c40504528911f4abcba0aebbf97da0b29
Argie Barcena <barcenaargie@gmail.com>
2022-02-05 12:19:43 +0800

    Finalizing css design, implementing rich text

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new'
+  get '/articles/:id' => 'articles#show', as: 'article_show'
   post '/articles' => 'articles#create', as: 'article_create'
   get '/articles/edit/:id' => 'articles#edit', as: 'article_edit'
   put '/articles/:id' => 'articles#update', as: 'article_update'
